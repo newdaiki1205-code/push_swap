f210c8785e93802290165466500319374f902b14
newdaiki1205-code <newdaiki1205@gmail.com>
2025-12-17 15:50:03 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -71,28 +71,28 @@ int	main(int ac, char **av)
 {
 	sort_array	*array;
 	map_array	*look_up;
-	int			size;
-	int 		check;
+	//int 		check;
 	t_list		*a;
 	t_list		*b;
 
 	if (ac < 2)
 		return (write(1, "\n", 1), 1);
-	size = ac - 1;
-	array = make_array(av, size);
+	array = make_array(av, ac - 1);
 	if (!array)
 		return (write(2, "Error\n", 6), -1);
-	look_up = make_look_up(array, size);
+	look_up = make_look_up(array, ac - 1);
 	if (!look_up)
 		return (free(array), write(2, "Error\n", 6), -1);
-	if (!is_it_sorted(look_up, size))
+	if (!is_it_sorted(look_up, ac - 1))
 		return (free(look_up), 1);
-	a = make_stack_a(look_up, size);
+	a = make_stack_a(look_up, ac - 1);
 	if (!a)
 		return (free(look_up), write(2, "Error\n", 6), -1);
 	b = NULL;
-	check = radix_sort(&a, &b, size);
-	if(check < 0)
+	// check = radix_sort(&a, &b, ac - 1);
+	// if(check < 0)
+	// 	return 1;
+	if(radix_sort(&a, &b, ac -1) < 0)
 		return 1;
 	return (free_list(&a), free_list(&b), 0);
 }
